fbe54844cbf02458764ae25103472dfe65865dda
LAVAKUMAR-P <84187714+LAVAKUMAR-P@users.noreply.github.com>
2021-10-21 20:39:08 +0530

    date logic added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const cors=require("cors");
 const PORT=3001;
+const fs=require('fs')
 
 app.use(
     cors({
@@ -10,6 +11,13 @@ app.use(
 )
 
 app.use(express.json());
+
+app.post("/create-file",function (req,res) {
+    fs.writeFile(`./demo.txt`,`${new Date()}`,function (err) {
+        if(err) return res.json(err)
+    })
+    res.json("File created")
+})
 
 app.listen(PORT,function(){
     console.log(`server is listening ${PORT}`);
